354f6656665ea0de50be9fc01095d936d13046d9
danyel-dev <danyelpinheiro154@hotmail.com>
2021-10-02 17:41:42 -0300

    prepopulated_fields adicionado no Slug

--- Courses/admin.py
+++ Courses/admin.py
@@ -9,7 +9,9 @@ class CourseAdmin(admin.ModelAdmin):
     list_editable = ('slug_course',)
     list_per_page = 10
     search_fields = ('id', 'name_course', 'slug_course')
-    
+
+    prepopulated_fields = {'slug_course': ('name_course',)}
+
 
 admin.site.register(Course, CourseAdmin)
 
