c83c08baaf9462899113252b8f7c622c855e86f9
danyel-dev <danyelpinheiro154@hotmail.com>
2021-10-02 11:27:25 -0300

    model de cursos criado

--- Courses/models.py
+++ Courses/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name_course = models.CharField(max_length=100, verbose_name='Nome do curso')
+    
+    slug_course = models.SlugField(verbose_name='Atalho')
+    
+    description_course = models.TextField(verbose_name='Descrição do curso')
+    
+    image_course = models.ImageField(upload_to='course_img/%Y/%m/%d', verbose_name='Imagem')
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Criado em')
+    
+    update_at = models.DateTimeField(auto_now=True, verbose_name='Atualizado em')
