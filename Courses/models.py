ef759c2063f12a593f11331bf5d7392be189f5a5
danyel-dev <danyelpinheiro154@hotmail.com>
2021-10-02 15:39:00 -0300

    Customizando o gerenciador de modelos

--- Courses/models.py
+++ Courses/models.py
@@ -1,15 +1,30 @@
 from django.db import models
 
 
+class CourseManager(models.Manager):
+
+    def search(self, query):
+        return self.get_queryset().filter(
+            models.Q(name_course__icontains = query) | 
+            models.Q(description_course__icontains = query)
+        )
+
+
 class Course(models.Model):
     name_course = models.CharField(max_length=100, verbose_name='Nome do curso')
     
     slug_course = models.SlugField(verbose_name='Atalho')
     
-    description_course = models.TextField(verbose_name='Descrição do curso')
+    description_course = models.TextField(verbose_name='Descrição do curso', blank=True)
     
-    image_course = models.ImageField(upload_to='course_img/%Y/%m/%d', verbose_name='Imagem')
+    image_course = models.ImageField(upload_to='course_img/%Y/%m/%d', verbose_name='Imagem', null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Criado em')
     
     update_at = models.DateTimeField(auto_now=True, verbose_name='Atualizado em')
+
+    objects = CourseManager()
+    
+
+    def __str__(self):
+        return self.name_course
