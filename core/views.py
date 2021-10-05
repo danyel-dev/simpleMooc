7d00bc1366040ac81103361516db76e57ac06725
danyel-dev <danyelpinheiro154@hotmail.com>
2021-10-05 16:46:32 -0300

    a

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from Courses.models import Course 
+
 
 def home(request):
     return render(request, 'home.html', {'home': 'home'})
@@ -8,3 +10,8 @@ def home(request):
 
 def contact(request):
     return render(request, 'contact.html', {'contact': 'contact'})
+
+
+def courses(request):
+    courses = Course.objects.order_by('-id')
+    return render(request, 'courses.html', {'courses': courses})
