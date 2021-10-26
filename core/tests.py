ab071bb4892e864a0c3dadd04e9234cf7cd55469
danyel-dev <danyelpinheiro154@hotmail.com>
2021-10-26 11:12:54 -0300

    fazendo testes nas views

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.test.client import Client
 
-# Create your tests here.
+
+class HomeViewTest(TestCase):
+    
+    def test_home_status_code(self):
+        client = Client()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_home_template_used(self):
+        client = Client()
+        response = client.get('/')
+        self.assertTemplateUsed(response, 'core/home.html')
+        self.assertTemplateUsed(response, 'base.html')
