f029f54deee3184b8f6c82a17306477bfabb4c9c
danyel-dev <danyelpinheiro154@hotmail.com>
2021-10-31 11:31:01 -0300

    app forum criado

--- simpleMooc/settings.py
+++ simpleMooc/settings.py
@@ -32,9 +32,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # apps
     'core',
     'Courses',
-    'Accounts',
+    'Accounts', 
+    'forum',
+    # libs
+    'taggit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
