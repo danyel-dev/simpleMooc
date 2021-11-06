0c87a8576560cb53ccbf72f4fcc44f7c2ec3a6fe
danyel-dev <danyelpinheiro154@hotmail.com>
2021-11-06 11:01:07 -0300

    paginaçao criada

--- forum/views.py
+++ forum/views.py
@@ -7,5 +7,10 @@ from .models import Topic
 class forum(ListView):
 
     model = Topic
-    paginate_by = 10
+    paginate_by = 1
     template_name = 'forum/list_topics.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(forum, self).get_context_data(**kwargs)
+        context['tags'] = Topic.tags.all()
+        return context
