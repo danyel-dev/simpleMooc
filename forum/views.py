a14b00ab4747fdee62742cfb87d96ca58eaa9ba1
danyel-dev <danyelpinheiro154@hotmail.com>
2021-11-06 11:47:13 -0300

    ordenaçao dos topicos completa

--- forum/views.py
+++ forum/views.py
@@ -10,6 +10,16 @@ class forum(ListView):
     paginate_by = 1
     template_name = 'forum/list_topics.html'
 
+    def get_queryset(self):
+        queryset = Topic.objects.all()
+
+        if 'order' in self.request.GET:
+            order = '-' + self.request.GET.get('order')
+            queryset = queryset.order_by(order)
+
+        return queryset
+
+
     def get_context_data(self, **kwargs):
         context = super(forum, self).get_context_data(**kwargs)
         context['tags'] = Topic.tags.all()
